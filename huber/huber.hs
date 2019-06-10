ab8361cb6271039c697219a7e8102e493b96be9e
ElDieguii <49466415+ElDieguii@users.noreply.github.com>
2019-06-09 22:35:41 -0300

    Update huber.hs

--- huber/huber.hs
+++ huber/huber.hs
@@ -1,7 +1,7 @@
 data Chofer = UnChofer{
     nombreChofer::String,
     kilometraje::Float,
-    viajesRealizados::Int
+    viajesRealizados::[Viaje],
     condicionParaTomarViaje::(Viaje->Bool)
 }
 
@@ -15,6 +15,42 @@ data Viaje = UnViaje{
 
 data Cliente = UnCliente{
     nombreCliente::String,
-    direccion :: String
+    barrio :: String
 }
 
+viajeSaleMasDe::Float->Viaje->Bool
+viajeSaleMasDe precio unViaje =
+    costo unViaje >= precio
+
+cantidadDeLetrasDelNombreDelClienteMayorA:: Int->Viaje->Bool
+cantidadDeLetrasDelNombreDelClienteMayorA letras unViaje=
+    (>letras).length.nombreCliente.cliente $unViaje
+
+clienteViveEn::String->Viaje->Bool
+clienteViveEn unBarrio unViaje =
+    (== unBarrio).barrio.cliente $unViaje
+
+lucas =UnCliente{
+    nombreCliente = "Lucas",
+    barrio ="Victoria"
+}
+daniel = UnChofer{
+    nombreChofer = "Daniel",
+    kilometraje = 23500,
+    viajesRealizados = [UnViaje{fecha = 20042017, cliente = lucas, costo = 150}],
+    condicionParaTomarViaje = (not.clienteViveEn "Olivos")
+}
+alejandra = UnChofer{
+    nombreChofer = "Alejandra",
+    kilometraje = 180000,
+    viajesRealizados = [],
+    condicionParaTomarViaje = (\_->True)
+}
+
+choferPuedeTomarElViaje:: Viaje->Chofer->Bool
+choferPuedeTomarElViaje unViaje unChofer =
+    condicionParaTomarViaje unChofer unViaje
+
+liquidacionDeViajes::Chofer->Int
+liquidacionDeViajes unChofer = 
+    sum.viajesRealizados $unChofer
